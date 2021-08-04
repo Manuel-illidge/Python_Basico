55dd4de721cab33b4b4c6133a0dfc4d8ad9fc894
Manuel-Illidge <orion8819@hotmail.com>
2021-08-04 11:35:55 -0500

    cambio de contenido por filosofia

--- pruebaAñadirgit.py
+++ pruebaAñadirgit.py
@@ -1,18 +1,13 @@
 """ escribiendo lo que sea para añadir al repositorio de
 
-probando añadir algo al repositorio de la 
+EVOLUCION DEL PENSAMIENDO DE LA PERSONA HUMANA:
 
+    Aunque los filosofos, y pensadores en general, coinciden en considerar al hombre como un ser peculiar dentro del mundo y,
+    más concretamente, dentro del reino animal, las concepciones que de la naturaleza humana se han expuesto difieren entre si.
 
-añadiendo contenido para enviar github
-anaiendo
+    En el pensamiento hebreo del Antiguo Testamento(los libros de este son redactados en un largo periodo, entre el siglo X y I a. de C),
+    el hombre es llamado indistrintamente alma
 
-probando con mas contenido para enviar github
-Contenido adicional, para git/github repo
-cambio adicional para probar vim
-
-
-otro cambio para probar ssh conection 
 
  """
-
 
